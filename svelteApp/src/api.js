f8a7a37ec30af2c10995efdb4f8a01bc0f5889b2
FGarciaVarela <pancho.garcia@uc.cl>
2024-06-04 12:53:22 -0400

    .env agregado solo para apikey

--- svelteApp/src/api.js
+++ svelteApp/src/api.js
@@ -1,8 +1,9 @@
 //Hecho con ayuda de chat GPT, y en base a documentación de https://www.nps.gov/subjects/developer/api-documentation.htm#/parks/getPark
 import { writable } from "svelte/store";
+let API_KEY = import.meta.env.VITE_API_KEY;
+console.log(API_KEY);
 
 export const parks = writable([]);
-export const activities = writable({});
 export const isLoading = writable(false);
 export const errorMessage = writable("");
 
@@ -12,7 +13,7 @@ export const events = writable([]);
 export async function fetchParks() {
   isLoading.set(true);
   errorMessage.set("");
-  const apiKey = 'XdeUjHZlivfLan4zHuuEQ6aXIICsUoMXdU8Qq9eb';
+  const apiKey = API_KEY;
   try {
     const response = await fetch(`https://developer.nps.gov/api/v1/parks?limit=50&api_key=${apiKey}`);
     if (!response.ok) {
@@ -29,30 +30,8 @@ export async function fetchParks() {
   }
 }
 
-export async function fetchActivitiesForPark(parkCode) {
-    isLoading.set(true);
-    errorMessage.set("");
-    const apiKey = 'XdeUjHZlivfLan4zHuuEQ6aXIICsUoMXdU8Qq9eb';
-    try {
-      const response = await fetch(`https://developer.nps.gov/api/v1/activities/parks?parkCode=${parkCode}&api_key=${apiKey}`);
-      if (!response.ok) {
-        throw new Error('Network response was not ok for activities');
-      }
-      const data = await response.json();
-      activities.update(currentActivities => {
-        currentActivities[parkCode] = data.data;
-        return currentActivities;
-      });
-    } catch (error) {
-      console.error(`Failed to fetch activities for park ${parkCode}:`, error);
-      errorMessage.set(`Failed to load activities for park ${parkCode}. Please try again later.`);
-    } finally {
-      isLoading.set(false);
-    }
-  }
-
   export async function fetchEvents(parkCode) {
-    const apiKey = 'XdeUjHZlivfLan4zHuuEQ6aXIICsUoMXdU8Qq9eb'; 
+    const apiKey = API_KEY; 
     const url = `https://developer.nps.gov/api/v1/events?parkCode=${parkCode}&api_key=${apiKey}`;
 
     isLoading.set(true);
@@ -64,12 +43,8 @@ export async function fetchActivitiesForPark(parkCode) {
         throw new Error('Failed to fetch events');
       }
 
-
       const data = await response.json();
-      events.update(currentEvents => {
-        currentEvents[parkCode] = data.data;
-        return currentEvents;
-      });
+      events.set(data.data);
     } catch (error) {
       console.error('Error:', error);
       errorMessage.set('Failed to load events. Please try again later.');
